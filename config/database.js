118f5617318be20d6314cda8032b93148f5e5269
jmatute <jmatute@limonbyte.com>
2021-08-30 14:52:10 -0400

    migrando api

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.hj6ya.mongodb.net'),
+        host: env('DATABASE_HOST', 'canguro-venezuela.95fhb.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'cangurove'),
-        username: env('DATABASE_USERNAME', 'luis'),
-        password: env('DATABASE_PASSWORD', 'N2JBtf3bVH9jNVvG'),
+        username: env('DATABASE_USERNAME', 'canguro'),
+        password: env('DATABASE_PASSWORD', '@RUhrFGQ&*Bie@wKJmbJQN@4ylBQRA'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'strapi',
+      cwd: '/var/www/html/strapi-canguro',
+      script: 'npm',
+      args: 'start',
+    },
+  ],
+};
